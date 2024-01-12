23db980c08600861309c2e53fac7c3b8fdb2d43f
Tamicktom <henrique.angelo@enterscience.com.br>
2024-01-11 22:17:06 -0300

    asdasd

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable } from 'node:stream';
+import { Readable, Writable } from 'node:stream';
 
 class OneToHundredStream extends Readable {
   index = 1;
@@ -17,4 +17,18 @@ class OneToHundredStream extends Readable {
   }
 }
 
-new OneToHundredStream().pipe(process.stdout);
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+    // const number = Number(chunk.toString());
+    // const result = number * 10;
+
+    // setTimeout(() => {
+    //   process.stdout.write(`${result}\n`);
+    //   callback();
+    // }, 1000);
+  }
+}
+
+new OneToHundredStream().pipe(new MultiplyByTenStream());
