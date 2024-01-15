2e96622448f8f1203daf22e4ced6cf96412870ba
Tamicktom <rickpokemon38@gmail.com>
2024-01-15 16:09:26 -0300

    saving

---
+++ streams/fake-upload-to-http-stream.js
@@ -0,0 +1,24 @@
+import { Readable } from 'node:stream';
+
+class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const number = this.index++;
+
+    setTimeout(() => {
+      if (number > 100) {
+        this.push(null);
+      } else {
+        const buffer = Buffer.from(`${number}\n`, 'utf-8');
+        this.push(buffer);
+      }
+    }, 1000);
+  }
+}
+
+fetch('http://localhost:3334', {
+  method: 'POST',
+  body: new OneToHundredStream(),
+  duplex: "half"
+});
